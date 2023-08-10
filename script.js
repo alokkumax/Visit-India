08c4f540891da2eee19c85635e8e5a5952a7beed
alokkumax <alokkumax1@gmail.com>
2023-08-10 11:44:32 +0530

    added tabs by functional method

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-// $("div-hero") = divs;
-// document.querySelectorAll(".hero-bottom-tab") = tabs;
-console.log( document.querySelectorAll(".hero-bottom-tab") )
+console.log(document.querySelectorAll(".hero-content"));
+console.log(document.querySelectorAll(".hero-content"));
 
-const tabs = [];
-
-document.querySelectorAll(".hero-content").forEach((item)=>{
-    // console.log(item.getAttribute("id"));
-    tabs.push(item.getAttribute("id"))
-
-})
-console.log(push);
-
-console.log("hwey")
-   
 $(".hero-bottom-tab").click(function(e){
-        
         console.log($(this).html());
-
-        showDiv($(this).html());
-        $(this).addClass("active-bottom-tab");
-
-
-        
+        updateDiv($(this).html());
+        updateActiveTab($(this))
 })
-function showDiv(clickedHTML){
+function updateDiv(clickedHTML){
+    var currDiv = $(".active-hero-content");
+console.log(currDiv)
+
+var currTab = $(".active-bottom-tab");
+console.log(currTab)
+
     $("#"+clickedHTML).removeClass("hero-content");
     $("#"+clickedHTML).addClass("active-hero-content");
+
+    currDiv.removeClass("active-hero-content")
+    currDiv.addClass("hero-content")
+
+    console.log("prev div "+currDiv.attr("id"))
+
+
+    currDiv = $(".active-hero-content").attr("id");
+    console.log("new div "+currDiv)
+
+
 }
-function removeActiveDiv(clickedHTML){
-   
+function updateActiveTab(clickedTab){
+    var currDiv = $(".active-hero-content");
+    console.log(currDiv)
+    
+    var currTab = $(".active-bottom-tab");
+    console.log(currTab)
+
+    $(clickedTab).removeClass("hero-bottom-tab");
+    $(clickedTab).addClass("active-bottom-tab");
+
+    currTab.removeClass("active-bottom-tab")
+    currTab.addClass("hero-bottom-tab")
+
+    console.log("prev div "+currTab.html())
+
+
+    currTab = $(".active-bottom-tab").html();
+        console.log("new tab "+currTab)
+        return currTab;
 
 }
